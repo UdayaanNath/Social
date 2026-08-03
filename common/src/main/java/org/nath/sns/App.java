afa8404801d53a5844a30fcf85c05c264fbe5e99
Udayaan <nathudayaan@gmail.com>
2026-08-03 17:17:14 +0530

    add common submodule

---
+++ common/src/main/java/org/nath/sns/App.java
@@ -0,0 +1,13 @@
+package org.nath.sns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
