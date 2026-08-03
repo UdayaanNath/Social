1d24f3190a9d2a4534a11735ff380938153a2c3a
Udayaan <nathudayaan@gmail.com>
2026-08-03 17:31:28 +0530

    feat: add Identity submodule

---
+++ Identity/src/main/java/org/nath/sns/App.java
@@ -0,0 +1,12 @@
+package org.nath.sns;
+
+/**
+ * Hello world!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
