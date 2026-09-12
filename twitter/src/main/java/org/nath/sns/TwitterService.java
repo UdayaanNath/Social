e11fc250fdb83b7492611e0ff3cd751523839663
Udayaan <nathudayaan@gmail.com>
2026-09-12 16:31:45 +0530

    feat: init twitter submodule and remove dependency-reduced-pom.xml

---
+++ twitter/src/main/java/org/nath/sns/TwitterService.java
@@ -0,0 +1,13 @@
+package org.nath.sns;
+
+/**
+ * Hello world!
+ *
+ */
+public class TwitterService
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
